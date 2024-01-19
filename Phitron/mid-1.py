3b1eae82511a11254cd328613aa8271c5210982d
SaminSadik <Saminsadikkhan@gmail.com>
2024-01-19 17:27:14 +0600

    Updated

--- Phitron/mid-1.py
+++ Phitron/mid-1.py
@@ -5,31 +5,40 @@ class Star_Cinema:
         self.__hall_list.append(hall)
 
 class Hall(Star_Cinema):
-    def __init__(self, rows, cols, halls):
+    def __init__(self, rows, cols, hall):
         self.__seats = {}
         self.__show_list = ()
         self.__rows = rows
         self.__cols = cols
-        self.__hall_no = halls
+        self.__hall_no = hall
+        self._booked = {}
         super()._entry_hall(self)
 
-    def entry_show(self, id, movie, time):     
-        self.__show_list.append((id, movie, time))
+    def entry_show(self, id, movie_name, time):     
+        self.__show_list.append((id, movie_name, time))
         self.__seats[id] = self.generate_seats(self.__rows, self.__cols)
 
     def book_seats(self, id):
-        #? check if seat is invalid or booked
-        while(True):
-            cell = input('Enter an available seat to book: ')
-            row = ord(cell[0]) - ord('A')
-            col = ord(cell[1]) - ord('0')
-            if (len(cell)>2 or row<0 or row>4 or col<0 or col>5):
-                print('Invalid Entry!')
-                self.view_available_seats(id)
-                continue
-            self.__seats[id][row][col] = f'[{cell}]'
-            break
-        #? take personal info & confirm
+        n = int(input('How many seats would you like to book: '))
+        if n==0: return
+        print("Great! Let's proceed one by one")
+        booked_seats = ()
+        for i in range(n):
+            while(True):
+                cell = input('Enter an available seat to book: ')
+                row = ord(cell[0]) - ord('A')
+                col = ord(cell[1]) - ord('0')
+                if (len(cell)>2 or row<0 or row>4 or col<0 or col>5 or (cell in self._booked[id])):
+                    print('Invalid Entry!')
+                    self.view_available_seats(id)
+                    continue
+                self.__seats[id][row][col] = f'[{cell}]'
+                if id not in self._booked:
+                    self._booked[id] = []
+                self._booked[id].append(cell)
+                booked_seats.append(cell)
+                break
+        self.make_invoice(booked_seats)
 
     def view_show_list(self):
         print('List of Ongoing Shows:')
@@ -59,6 +68,21 @@ class Hall(Star_Cinema):
                 row_values.append(cell_value)
             matrix.append(row_values)
         return matrix
+    
+    def make_invoice(list):
+        print("Please provide necessary info for the invoice:")
+        nam = input("Name - ")
+        num = input("Contact Number - ")
+        print('-------------------------------')
+        print('Booking Successful!')
+        print(f'Customer Name    : {nam}')
+        print(f'Customar Contact : {num}')
+        if(len(list)>1): print('Booked Seat      :', end=" ")
+        else: print('Booked Seats     :', end=" ")
+        for seat in list:
+            print(seat, end=' ')
+        #? show movie id, name, time, hall
+        print('-------------------------------')
 
 
 print('-------------------------------')
@@ -79,6 +103,7 @@ while(True):
         print('Thank you for visiting Star Cinema. Come again!')
         break
 
+    #? take hall_no as input?
     customer = Hall(5,6,'A1')
 
     if op == '4':
