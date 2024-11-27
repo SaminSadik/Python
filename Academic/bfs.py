497fc7e820b70e29eff0e976ff79eae291a57197
Samin Sadik Khan <124756396+SaminSadik@users.noreply.github.com>
2024-11-27 21:38:09 +0600

    Update bfs.py

--- Academic/bfs.py
+++ Academic/bfs.py
@@ -1,35 +1,24 @@
 def bfs(graph, start):
-    visited = set()
-    queue = [start]
-    visited.add(start)
-    
+    visited, queue = set([start]), [start]
     while queue:
         node = queue.pop(0)
         print(node, end=" ")
-        
         for neighbour in graph[node]:
             if neighbour not in visited:
                 visited.add(neighbour)
                 queue.append(neighbour)
 
-def graph_input():
-    graph = {}
-    edges = int(input("Enter the number of edges: "))
-    
+def graph_input(graph = {}):
+    edges = int(input("Enter the number of edges in your graph: "))
+    print("Enter the Edges (node1 node2): ")
     for _ in range(edges):
         u, v = input().split()
-        if u not in graph:
-            graph[u] = []
-        if v not in graph:
-            graph[v] = []
-        graph[u].append(v)
-        graph[v].append(u)
-    
+        graph.setdefault(u, []).append(v)
+        graph.setdefault(v, []).append(u)
     return graph
 
 graph = graph_input()
 start = input("Enter the starting node for BFS: ")
-
-print("Breadth-First Search Traversal:")
+print("Breadth-First Search Traversal: ")
 bfs(graph, start)
 print()
